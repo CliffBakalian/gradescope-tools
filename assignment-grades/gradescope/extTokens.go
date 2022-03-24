dbba313b502b0bbbe93aa7ae78a604f0c9688406
cyb <cliffbakalian@gmail.com>
2022-03-23 23:22:51 -0400

    added token per assignment field

--- assignment-grades/gradescope/extTokens.go
+++ assignment-grades/gradescope/extTokens.go
@@ -23,6 +23,7 @@ type Student struct{
   UID string `json:"UID"`
   Tokens int `json:"tokens"`
   Assignments []string `json:"assignments"`
+  AssignTokens []int `json:"assignTokens`
 }
 
 func readExtensions() TokenList{
@@ -100,6 +101,7 @@ func updateExtensions(submissions []Submission, tokens TokenList, assignID strin
     //update information
     tokenNumber := (submission.late.hour / 12)+1
     oldTokens := currStudent.Tokens
+    currStudent.AssignTokens = append(currStudent.AssignTokens,int(tokenNumber))
     currStudent.Tokens = currStudent.Tokens + int(tokenNumber)
     currStudent.Assignments = append(currStudent.Assignments,assignID)
     fmt.Printf("%s %s (%s) had %d, submitted %d hours late, and has used %d tokens\n",submission.fname, submission.lname,currStudent.UID,oldTokens,submission.late.hour,int(currStudent.Tokens))
