afe2456a4bd447aa759370df1ab03355e307d9d1
cliff <cliffbakalian@gmail.com>
2022-11-08 14:54:55 -0500

    changed some flags and stuff

--- grading-stats/main.go
+++ grading-stats/main.go
@@ -10,12 +10,13 @@ func main() {
   interactive := flag.Bool("interactive", false, "run in interactive mode")
   course_id := flag.String("course", "", "The course ID")
   assignment_id := flag.String("assignment","","The assignment ID")
-  email := flag.String("email","cliffbakalian@gmail.com","login email")
+  email := flag.String("email","","login email")
   password := flag.String("password","","please never use but helpful for scripting purposes")
-  all := flag.Bool("all", false, "scrape everything. May take a while")
-  cache := flag.Bool("cache", false, "don't scrape, but read from cache")
+  pf := flag.Bool("print", false, "pretty print output to stdout")
+  //partial := flag.Bool("partial", false, "update only those that are not graded")
+  update := flag.Bool("update", false, "update the databse")
 
   flag.Parse()
 
-  gradescope.Gradescope(*interactive, *course_id, *assignment_id, *email, *password, *all ,*cache)
+  gradescope.Gradescope(*interactive, *course_id, *assignment_id, *email, *password, *pf, *update)
 }
